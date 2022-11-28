f0ea3ea41dd172f6ce99422111a43f21fc44112a
xm-tech <maxm09@163.com>
2022-11-28 08:31:02 +0800

    rename

--- server/service/main.lua
+++ server/service/main.lua
@@ -1,12 +1,11 @@
 local skynet = require "skynet"
-local conf = require "system"
+-- will search system.lua in the config.lua_path
+local sys_conf = require "system"
 
 skynet.start(function ()
 	skynet.error("service main start")
 
 	-- start debug_console service
-	skynet.newservice("debug_console", conf.debug_console_port)
-
-	skynet.exit()
+	skynet.newservice("debug_console", sys_conf.debug_console_port)
 
 end)
