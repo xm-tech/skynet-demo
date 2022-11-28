5c281231ef2beabe8e6a82a36d526ccc35914e20
xm-tech <maxm09@163.com>
2022-11-28 08:15:45 +0800

    fix lua path

--- server/service/main.lua
+++ server/service/main.lua
@@ -1,11 +1,11 @@
 local skynet = require "skynet"
-local system_conf = require "conf.system"
+local conf = require "system"
 
 skynet.start(function ()
 	skynet.error("service main start")
 
 	-- start debug_console service
-	skynet.newservice("debug_console", system_conf.debug_console_port)
+	skynet.newservice("debug_console", conf.debug_console_port)
 
 	skynet.exit()
 
