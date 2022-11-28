43cca2018075ed96527d009e78cdbed411dceac5
xm-tech <maxm09@163.com>
2022-11-28 08:34:08 +0800

    fix comment

--- server/service/main.lua
+++ server/service/main.lua
@@ -5,7 +5,5 @@ local sys_conf = require "system"
 skynet.start(function ()
 	skynet.error("service main start")
 
-	-- start debug_console service
 	skynet.newservice("debug_console", sys_conf.debug_console_port)
-
 end)
